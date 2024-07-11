743ebdd1155b186a15ffc0b70cfbe45e02cd642b
guehlee <95629447+guehlee@users.noreply.github.com>
2024-07-11 14:56:50 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const videoContainer = document.getElementById("video-container");
 const button1 = document.querySelector(".button1");
 const button2 = document.querySelector(".button2");
 const button3 = document.querySelector(".button3");
+const button4 = document.querySelector(".button4");
+const button5 = document.querySelector(".button5");
+const button6 = document.querySelector(".button6");
 
 const MODEL_URI = "models";
 let mouthOpen = false;
